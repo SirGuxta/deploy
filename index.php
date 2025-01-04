bbffd338a7d08cb6b4f0a1398eaccfa95c316a4a
SirGuxta <96639033+SirGuxta@users.noreply.github.com>
2025-01-04 16:43:02 -0300

    meu primeiro commit

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caloteiro</title>
+</head>
+<body>
+    <img src="foto.png">
+    <h2>Pague o meu dinheiro, índio maldito!</h2>
+</body>
+</html>
+
+
+<style>
+
+body {
+    display: grid;
+    justify-items: center;
+    align-items: center;
+}
+
+body img {
+    width: 40vw;
+    border-radius: 100%;
+    
+}
+
+</style>
