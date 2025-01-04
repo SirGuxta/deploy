2118b814250b50c8a83f3e06bc6ba192ce393413
SirGuxta <96639033+SirGuxta@users.noreply.github.com>
2025-01-04 17:09:54 -0300

    v01

--- index.php
+++ index.php
@@ -24,7 +24,19 @@ body {
 body img {
     width: 40vw;
     border-radius: 100%;
-    
+
+    animation: girar 4s infinite;
+
+}
+
+@keyframes girar {
+    0% {
+        transform: rotate(0deg)
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
 }
 
 </style>
